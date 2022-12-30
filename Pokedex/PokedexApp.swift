417f685d1d600e3d2c3ea6d81f6e3646fd854cfe
Kevin van den Hoek <k.vandenhoek@wearetriple.com>
2022-12-30 13:17:09 +0100

    first commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kevin van den Hoek on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
